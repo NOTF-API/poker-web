7f579029c62cdb96b9680c98e0d1d37ad07bf6e0
NOTF-API <1764914014@qq.com>
2022-04-13 22:51:02 +0800

    增加样式

--- js/demo.js
+++ js/demo.js
@@ -18,38 +18,57 @@ addCardsEventListener();
 
 // 给自己的牌添加点击事件用于切换状态(selected <-> ...)
 function addCardsEventListener(){
-    const playerCards = document.querySelector(".player-self").querySelector(".cards").children
+    const playerCards = document.querySelector(".player-self").querySelector(".stacked").children
     for (let i = 0; i < playerCards.length; i++) {
         const item = playerCards[i]
-        item.addEventListener("click", (e) => {
-            if (e.target.classList.contains("selected")) {
-                e.target.classList.remove("selected")
-            }else{
-                e.target.classList.add("selected")
-            }
-            updatePosition();
-        })
+        addSingleItemEventListener(item);
     }
+}
+
+function addSingleItemEventListener(item){
+    item.addEventListener("click", (e) => {
+        if (e.target.classList.contains("selected")) {
+            e.target.classList.remove("selected")
+        }else{
+            e.target.classList.add("selected")
+        }
+        updatePosition();
+    })
 }
 
 // 用于把牌堆叠排放
 function updatePosition() {
-    const playerCards = document.querySelector(".player-self").querySelector(".cards").children
+    const playerCards = document.querySelector(".player-self").querySelector(".stacked").children
     for (let i = 0; i < playerCards.length; i++) {
         const item = playerCards[i]
-        let xoffset = i * 2 - (playerCards.length/2+1);
-        let yoffset = item.classList.contains("selected") ? 1 : 0
+        let xoffset = i * 2 - (playerCards.length-1);
+        let yoffset = item.classList.contains("selected") ? -1 : 0
         item.style.transform = `translate(${xoffset}rem,${yoffset}rem)`
     }
 }
 
 // 用于把公共堆叠排放
 function updatePublicPosition() {
-    const playerCards = document.querySelector(".public").querySelector(".cards").children
+    const el = document.querySelector(".public").querySelector(".stacked")
+    if(el===null)return
+    const playerCards = el.children
     for (let i = 0; i < playerCards.length; i++) {
         const item = playerCards[i]
-        let xoffset = i * 2 - (playerCards.length/2+1);
+        let xoffset = i * 2 - (playerCards.length-1);
         let yoffset = 0
         item.style.transform = `translate(${xoffset}rem,${yoffset}rem)`
     }
 }
+
+document.querySelectorAll("div").forEach(el=>{
+    // el.style.border="1px solid black"
+})
+
+document.querySelector(".idea").addEventListener("click",()=>{
+    const child = document.createElement("div");
+    child.className="normal heart card"
+    child.innerText="X"
+    addSingleItemEventListener(child)
+    document.querySelector(".player-self").querySelector(".stacked").appendChild(child)
+    updatePosition();
+})
